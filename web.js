cfc5c404e7aeaf09459d8176faac3fa0b7f79a45
Robert Maranga <rmaranga@gmail.com>
2013-07-14 20:38:49 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//read file from index.html using fs.readFileSync and read the generated buffer.
+
+var myindex = 'index.html';
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync(myindex));
+var helloworld = buf.toString('utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(helloworld);
 });
+
+// helloworld displays new text.
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
